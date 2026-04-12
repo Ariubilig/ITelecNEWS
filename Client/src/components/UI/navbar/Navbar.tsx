6bffdbe72540c950c8633dcf9435e358d59cb47a
Ariubilig <arierdene0@gmail.com>
2026-04-12 11:45:26 +0800

    Adjust, +Server TS configuration ahh

--- Client/src/components/UI/navbar/Navbar.tsx
+++ Client/src/components/UI/navbar/Navbar.tsx
@@ -2,18 +2,21 @@ import './Navbar.css'
 
 
 function Navbar() {
+    
 
     return (
         <div className="navbar">
+
             <div className="navbar-logo">
                 <h1>ITelecNEWS</h1>
             </div>
+
             <div className="navbar-links">
                 <a href="/">Home</a>
             </div>
+            
         </div>
     )
-
 }
 
 
