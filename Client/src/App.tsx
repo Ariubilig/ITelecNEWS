0b461a797c0636122586664af4b50cb03ff4f13d
Ariubilig <arierdene0@gmail.com>
2026-05-02 19:49:29 +0800

    Depoy error linux f " `

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -4,8 +4,8 @@ import { useRef } from "react";
 
 import Home from "./pages/home/Home";
 import Reading from './pages/reading/Reading';
-import Navbar from "./components/ui/navbar/Navbar";
-import ScrollBar from "./components/ui/ScrollBar/ScrollBar";
+import Navbar from './components/ui/navbar/Navbar';
+import ScrollBar from './components/ui/ScrollBar/ScrollBar';
 
 import { useScrollSmoother } from "./hooks/useScrollSmoother";
 import { useFontsReady } from "./hooks/useFontsReady";
